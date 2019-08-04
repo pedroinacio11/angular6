1db5f53b979b786159fc80584c8fd6c624ebdfd8
pedro-inacio11 <pedro.inacio11@gmail.com>
2019-08-04 20:59:39 -0300

    Implementing Menu Items

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,9 +6,11 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 import {Restaurant} from "./restaurant/restaurant.model"
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 import {MEAT_API} from '../app.api'
 import {ErrorHandler} from '../app.error-handler'
+
 
 @Injectable()
 export class RestaurantsService {
@@ -23,6 +25,18 @@ export class RestaurantsService {
 
     restaurantById(id: string): Observable<Restaurant>{
       return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
     }
